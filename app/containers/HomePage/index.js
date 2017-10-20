a9d2fc2de4fca879dbfd12c3e0df44cab83a3f56
Andrew Russell <arussell314@yahoo.com>
2017-10-19 23:50:27 -0500

    'andrewsFirstChanges'

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -50,7 +50,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
     return (
       <article>
         <Helmet>
-          <title>Home Page</title>
+          <title>I changed the Home Page :)</title>
           <meta name="description" content="A React.js Boilerplate application homepage" />
         </Helmet>
         <div>
